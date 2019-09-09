464f625814c444ac6465ace95c45de39583772b0
aakash1122 <aakaashhossain@gmail.com>
2019-09-09 17:07:47 +0600

    Updates

---
+++ precache-manifest.663f8cbb17af90603f52d5fff5acfc1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9478a9a770cf9053e1334403b861246a",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "253dd10c7895ad83893f",
+    "url": "/tic-tac-toe/static/css/main.4c3292c4.chunk.css"
+  },
+  {
+    "revision": "d772f56844bd0020839b",
+    "url": "/tic-tac-toe/static/js/2.faec245a.chunk.js"
+  },
+  {
+    "revision": "253dd10c7895ad83893f",
+    "url": "/tic-tac-toe/static/js/main.0b6bb5d4.chunk.js"
+  },
+  {
+    "revision": "091a9f2b7b982cee2e93",
+    "url": "/tic-tac-toe/static/js/runtime~main.40bb1757.js"
+  }
+]);
